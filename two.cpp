b2b02d8da29de2c0745891b281623292fa80e7a9
Sonia <sonchas3305@gmail.com>
2024-09-21 21:23:55 +0300

    6

--- two.cpp
+++ two.cpp
@@ -4,5 +4,5 @@ int main() {
     std::cout << "Hello, world!\n";
     std::cout << "How are you?\n";
     std::cout << "I'm fine, and you?\n";
-    std::cout << "I'm not doing well.\n";
+    std::cout << "I'm not doing well. oh...\n";
 }
